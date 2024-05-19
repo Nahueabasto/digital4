78b25402ac7da7dd8224c172223bcab793416178
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-05-18 23:21:26 -0300

    fsf

--- Front/src/api/axios.js
+++ Front/src/api/axios.js
@@ -7,7 +7,7 @@ if (window.location.hostname === 'localhost') {
     baseURL = 'http://localhost:3001';
 } else {
     // Si no es localhost, asume que está en producción y utiliza la URL de producción
-    baseURL = 'https://diario-digital.vercel.app/';
+    baseURL = 'https://digital4-xlhr.vercel.app/';
 }
 
 // Crea una instancia de axios con la URL correspondiente
